3340c134a48e499d8f597c3c74062f9fb0ac905e
PC 12 <rizkypertiwi2209@gmail.com>
2022-03-14 16:38:58 +0700

    Inintial project Penamaan variabel

---
+++ src/Variabel.java
@@ -0,0 +1,9 @@
+public class Variabel {
+    public static void main(String[] args) {
+        double salary;
+        long _distance;
+        boolean $flag;
+        /**Contoh penamaan variabel pada bahasa java
+
+    }
+}
